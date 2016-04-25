b3998a8a636a77242289906725e8d88f3f10ea7c
gonczor <wiktor.gonczaronek@gmail.com>
2016-04-25 13:06:11 +0200

    added interpreter to the beginning of main

--- AIDS-y/grafy/main.py
+++ AIDS-y/grafy/main.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 from DataStructure import Line, Matrix
 from Algorithm import BroadFirstSearch, DepthFirstSearch
 
 
 class Analyze:
     def __init__(self):
-        matrix = Matrix.Matrix(5, [[0, 1]])
+        matrix = Matrix.Matrix(5, [[0, 1], [3, 4]])
         matrix.show()
 
 if __name__ == '__main__':
