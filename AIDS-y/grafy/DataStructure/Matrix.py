3ca03707a6d25d0b160c6070563dade448ed7e41
gonczor <wiktor.gonczaronek@gmail.com>
2016-04-27 11:40:12 +0200

    Extended matrix class.

--- AIDS-y/grafy/DataStructure/Matrix.py
+++ AIDS-y/grafy/DataStructure/Matrix.py
@@ -2,14 +2,20 @@ import DataStructure.DataStructure as DataStructure
 from colorama import init, Fore
 
 
+class Node:
+    def __init__(self):
+        self.value = False
+        self.checked = False
+
+
 class Matrix(DataStructure.DataStructure):
     def __init__(self, number_of_elements, connections):
         super().__init__(number_of_elements, connections)
-        self.__matrix__ = [[False for i in range(self.__number_of_elements__)]
+        self.__matrix__ = [[Node() for i in range(self.__number_of_elements__)]
                            for j in range(self.__number_of_elements__)]
         for connection in self.__connections__:
-            self.__matrix__[connection[0]][connection[1]] = True
-            self.__matrix__[connection[1]][connection[0]] = True
+            self.__matrix__[connection[0]][connection[1]].value = True
+            self.__matrix__[connection[1]][connection[0]].value = True
 
     def create(self):
         pass
@@ -18,8 +24,8 @@ class Matrix(DataStructure.DataStructure):
         init(autoreset=True)
         for row in self.__matrix__:
             for cell in row:
-                if cell:
-                    print(Fore.GREEN + "{0}".format(cell), end="\t")
+                if cell.value:
+                    print(Fore.GREEN + "{0}".format(cell.value), end="\t")
                 else:
-                    print(Fore.RED + "{0}".format(cell), end="\t")
+                    print(Fore.RED + "{0}".format(cell.value), end="\t")
             print()
