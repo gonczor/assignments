7cf1a89069e32c4f0f4e367d12030bf82ad94530
gonczor <wiktor.gonczaronek@gmail.com>
2016-04-27 20:52:08 +0200

    Made matrix field in Matrix public

--- AIDS-y/grafy/DataStructure/Matrix.py
+++ AIDS-y/grafy/DataStructure/Matrix.py
@@ -11,21 +11,30 @@ class Node:
 class Matrix(DataStructure.DataStructure):
     def __init__(self, number_of_elements, connections):
         super().__init__(number_of_elements, connections)
-        self.__matrix__ = [[Node() for i in range(self.__number_of_elements__)]
-                           for j in range(self.__number_of_elements__)]
+        self.matrix = [[Node() for i in range(self.__number_of_elements__)]
+                       for j in range(self.__number_of_elements__)]
         for connection in self.__connections__:
-            self.__matrix__[connection[0]][connection[1]].value = True
-            self.__matrix__[connection[1]][connection[0]].value = True
+            self.matrix[connection[0]][connection[1]].value = True
+            self.matrix[connection[1]][connection[0]].value = True
 
     def create(self):
         pass
 
     def show(self):
         init(autoreset=True)
-        for row in self.__matrix__:
+        for row in self.matrix:
             for cell in row:
                 if cell.value:
                     print(Fore.GREEN + "{0}".format(cell.value), end="\t")
                 else:
                     print(Fore.RED + "{0}".format(cell.value), end="\t")
             print()
+
+    def get_neighbours_indexes_(self, item_index):
+        neighbours = list()
+        for index in range(len(self.matrix[item_index])):
+            if (index != item_index and
+                    self.matrix[item_index][index].value and
+                    not self.matrix[item_index][index].checked):
+                neighbours.append(index)
+        return neighbours
